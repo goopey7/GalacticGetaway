3286b1d83fb87b2934b13a19c1ec3be51b27cf9e
Sam Collier <samcollier916@gmail.com>
2023-03-09 03:27:27 +0000

    start input action manager with classy macro initialization

---
+++ build/vs2017/InputActionManager.cpp
@@ -0,0 +1,42 @@
+﻿#include "InputActionManager.h"
+#include <fstream>
+
+InputActionManager::InputActionManager()
+{
+	// initialize actions vector from string
+	initializeActions(actionsStr);
+
+	// TODO handle error if file not found
+	
+	// read bindings.json in config folder
+	std::ifstream i("config/bindings.json");
+
+	// parse json into cpp object
+	bindings = json::parse(i);
+
+}
+
+void InputActionManager::handleInputEvents()
+{
+}
+
+void InputActionManager::initializeActions(const char* values)
+{
+	for(int i = 0; values[i] != '\0'; i++) {
+		if(values[i] == ',') {
+			actions.push_back(std::string(values, i));
+			values = values + i + 1;
+			i = -1;
+		}
+	}
+	actions.push_back(values);
+
+	// clear up spaces
+	for(int i = 0; i < actions.size(); i++)
+	{
+		if(actions[i][0] == ' ')
+		{
+			actions[i] = actions[i].substr(1);
+		}
+	}
+}
---
+++ build/vs2017/InputActionManager.h
@@ -0,0 +1,32 @@
+﻿#pragma once
+
+#include "json.h"
+
+using nlohmann::json;
+
+#define DEFINE_ACTIONS(...) \
+	enum Action { __VA_ARGS__ }; \
+	const char* actionsStr = #__VA_ARGS__; \
+
+DEFINE_ACTIONS(
+	MoveUp,
+	MoveDown,
+	MoveLeft,
+	MoveRight,
+	Jump,
+)
+
+class InputActionManager
+{
+public:
+	InputActionManager();
+	void handleInputEvents();
+	void initializeActions(const char* values);
+
+private:
+	json bindings;
+	std::map<Action,bool> actionMapPressed;
+	std::map<Action,bool> actionMapHeld;
+	std::map<Action,bool> actionMapReleased;
+	std::vector<std::string> actions;
+};
