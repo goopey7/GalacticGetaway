2c745617e6f110f6f0b88c5f39638fcd9a3349f2
Sam Collier <samcollier916@gmail.com>
2023-03-10 21:05:11 +0000

    support multiple keyboard bindings per action

--- build/vs2017/InputActionManager.cpp
+++ build/vs2017/InputActionManager.cpp
@@ -18,9 +18,13 @@ InputActionManager::InputActionManager(gef::Platform& platform)
 	bindingsJson = json::parse(i);
 
 	// setup action bindings
-	for(auto keyboardBinding : bindingsJson["keyboard"])
+	for(auto keyboardBinding : bindingsJson["actions"])
 	{
-		actionBindings[stringToKeyCode[keyboardBinding["key"]]] = stringToAction[keyboardBinding["action"]];
+		const Action action = stringToAction[keyboardBinding["action"]];
+		for(const auto& key : keyboardBinding["keys"])
+		{
+			actionBindings[stringToKeyCode[key]] = action;
+		}
 	}
 
 	inputManager = gef::InputManager::Create(platform);
@@ -45,12 +49,29 @@ void InputActionManager::Update()
 {
 	auto kb = inputManager->keyboard();
 	kb->Update();
-	
-	for(auto binding : actionBindings)
+
+	for(auto actionStr : actions)
 	{
-		actionMapPressed[binding.second] = kb->IsKeyPressed(binding.first);
-		actionMapHeld[binding.second] = kb->IsKeyDown(binding.first);
-		actionMapReleased[binding.second] = kb->IsKeyReleased(binding.first);
+		Action action = stringToAction[actionStr];
+
+		for(const auto binding : actionBindings)
+		{
+			if(action == binding.second)
+			{
+				bool oldMapPressed = actionMapPressed[action];
+				bool oldMapHeld = actionMapHeld[action];
+				bool oldMapReleased = actionMapReleased[action];
+				
+				actionMapPressed[action] = kb->IsKeyPressed(binding.first);
+				actionMapHeld[action] = kb->IsKeyDown(binding.first);
+				actionMapReleased[action] = kb->IsKeyReleased(binding.first);
+				
+				if(oldMapPressed != actionMapPressed[action] || oldMapHeld != actionMapHeld[action] || oldMapReleased != actionMapReleased[action])
+				{
+					break;
+				}
+			}
+		}
 	}
 }
 
