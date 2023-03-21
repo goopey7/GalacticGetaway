5ffa38433cb8897f1d1e9d7cd99ee9ddd83d5bfb
Sam Collier <samcollier916@gmail.com>
2023-03-21 13:48:26 +0000

    start reading level json file

---
+++ build/vs2017/Level.cpp
@@ -0,0 +1,56 @@
+﻿#include "Level.h"
+
+#include <fstream>
+
+#include "GameObject.h"
+#include "json.h"
+#include "primitive_builder.h"
+#include "box2d/b2_math.h"
+#include "box2d/b2_world.h"
+
+using nlohmann::json;
+
+Level::Level(gef::Platform& platform)
+	: platform_(&platform)
+{
+}
+
+void Level::LoadFromFile(const char* filename)
+{
+	// load level from file
+	std::ifstream i(std::string("levels/") + std::string(filename));
+
+	// handle error if file not found
+	if (i.fail())
+	{
+		throw std::exception((std::string(filename)+ " not found").c_str());
+	}
+	
+	// parse json into cpp object
+	json levelJson = json::parse(i);
+
+	// initialize all layers
+	for(const auto& layer : levelJson["layers"])
+	{
+		if (layer["type"] == "objectgroup")
+		{
+			for(const auto& obj : layer["objects"])
+			{
+				game_objects_.emplace_back(new GameObject());
+				game_objects_.back()->Init(obj["width"],obj["height"], 0.5f,obj["x"], obj["y"],b2_world_, primitive_builder_);
+			}
+		}
+	}
+
+	Init();
+}
+
+void Level::Init()
+{
+	// initialize box2d world
+	b2_world_ = new b2World(b2Vec2(0.0f, -9.8f));
+	
+	// initialise primitive builder to make create some 3D geometry easier
+	primitive_builder_ = new PrimitiveBuilder(*platform_);
+}
+
---
+++ build/vs2017/Level.h
@@ -0,0 +1,33 @@
+﻿#pragma once
+#include <vector>
+
+class GameObject;
+
+namespace gef
+{
+	class Platform;
+	class Renderer3D;
+	class Font;
+	class SpriteRenderer;
+}
+
+class b2World;
+class PrimitiveBuilder;
+
+class Level
+{
+public:
+	Level(gef::Platform& platform);
+	void LoadFromFile(const char* filename);
+private:
+	void Init();
+	b2World* b2_world_;
+	PrimitiveBuilder* primitive_builder_;
+	
+	gef::SpriteRenderer* sprite_renderer_;
+	gef::Font* font_;
+	gef::Renderer3D* renderer_3d_;
+	gef::Platform* platform_;
+
+	std::vector<GameObject*> game_objects_;
+};
